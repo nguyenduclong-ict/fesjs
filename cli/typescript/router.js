5863b1caf2271bb723b7810348acc74989f53dcb
Nguyễn Đức Long <longnd@rabiloo.com.vn>
2020-05-23 22:04:22 +0700

    fix cli generate router

--- cli/typescript/router.js
+++ cli/typescript/router.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
-const Path = require("path");
-const log = require("../log");
+const fs = require('fs')
+const Path = require('path')
+const log = require('../log')
 
 const SERVICE_FILE = `
 import { Request, Response, NextFunction } from 'express';
 export async function handle(req: Request, res: Response, next: NextFunction) {}
-`.trim();
+`.trim()
 
 const ROUTER_FILE = `
 import { Router } from 'express';
@@ -15,7 +15,7 @@ const router = Router();
 // ------- Declare router -------
 
 // ------------------------------
-export default router;`.trim();
+export default router;`.trim()
 
 const VALIDATOR_FILE = `
 import check from "check-types";
@@ -24,17 +24,17 @@ export const ValidateBody = CreateValidator(
     {},
     "body"
 );
-`.trim();
+`.trim()
 
 module.exports.generateRouter = function (path) {
-    const routerPath = Path.join("routes", path);
+    const routerPath = Path.join('routes', path)
     if (fs.existsSync(routerPath)) {
-        log.show(log.colors.FgRed)("router", routerPath, "exists");
-        return;
+        log.show(log.colors.FgRed)('router', routerPath, 'exists')
+        return
     }
 
-    fs.mkdirSync(routerPath, { recursive: true });
-    fs.writeFileSync(routerPath + "/service.ts", SERVICE_FILE);
-    fs.writeFileSync(routerPath + "/router.ts", ROUTER_FILE);
-    fs.writeFileSync(routerPath + "/router.ts", ROUTER_FILE);
-};
+    fs.mkdirSync(routerPath, { recursive: true })
+    fs.writeFileSync(routerPath + '/service.ts', SERVICE_FILE)
+    fs.writeFileSync(routerPath + '/router.ts', ROUTER_FILE)
+    fs.writeFileSync(routerPath + '/validator.ts', VALIDATOR_FILE)
+}
